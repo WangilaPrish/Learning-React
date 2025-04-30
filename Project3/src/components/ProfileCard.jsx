a774c4c2fd1fba895fea10c5ffd79e9b2c3afb2b
WangilaPrish <nabwonyaprish@gmail.com>
2025-04-30 14:42:57 +0300

    Styling in Reactjs. add main components: StyledCard, ProfileCard, and IconComponent.

---
+++ Project3/src/components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfileCard = () => {
+
+    const styles = {backgroundColor: "lightGray", padding: "15px", borderRadius: "8px", color: "black"}
+  return (
+      <div style={styles}>
+          <h1>Profile Card</h1>
+          <p>This is a simple profile card component.</p>
+      </div>
+  )
+}
+
+export default ProfileCard;
---
+++ Project3/src/components/StyledCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StyledCard = () => {
+  return (
+      <div style={{ backgroundColor: "lightblue", padding: "20px", borderRadius: "10px", color: "blue" }}>
+          <h1>Styled Card</h1>
+          <p>This is a simple styled card component.</p>
+      </div>
+  )
+}
+
+export default StyledCard;
