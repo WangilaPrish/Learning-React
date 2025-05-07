d608c97a6c7cd779b022df8165400f1280b22ede
WangilaPrish <nabwonyaprish@gmail.com>
2025-05-07 15:13:43 +0300

    HiddenSearchBar component with styling and functionality.

---
+++ Project11/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HiddenSearchBar from './components/HiddenSearchBar.jsx'
+
+
+const App = () => {
+  return (
+      <div>
+        <HiddenSearchBar />
+    </div>
+  )
+}
+
+export default App
