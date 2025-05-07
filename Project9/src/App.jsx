78b56a63a2ce04c0ec54a6efe418b5f479172967
WangilaPrish <nabwonyaprish@gmail.com>
2025-05-07 13:34:06 +0300

    add ToggleButton component with state management for dynamic styling.

---
+++ Project9/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToggleButton from './components/ToggleButton'
+
+const App = () => {
+  return (
+      <div>
+          <ToggleButton />
+    </div>
+  )
+}
+
+export default App
---
+++ Project9/src/components/ToggleButton.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const ToggleButton = () => {
+    const [backgroundColor, setBackgroundColor] = useState('white')
+    const [textColor, setTextColor] = useState('black')
+    const[buttonColor, setButtonColor] = useState('white')
+
+    return (
+      <div>
+          <h1 style={{ color: textColor, backgroundColor: backgroundColor }}>THIS IS MY SMALL EXERCISE</h1>
+            <button style={{ backgroundColor: buttonColor }} onClick={() => {
+                setBackgroundColor(backgroundColor === 'black' ? 'white' : 'black')
+                setTextColor(textColor === 'white' ? 'black' : 'white')
+                setButtonColor(buttonColor === 'red' ? 'white' : 'red')
+            }}>Click me</button>
+      </div>
+  )
+}
+
+export default ToggleButton
