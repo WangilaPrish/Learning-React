cdc839b783b44c7b2916a660785536b3853d0368
WangilaPrish <nabwonyaprish@gmail.com>
2025-05-08 14:07:26 +0300

    - Create main application component (App.jsx) to render accordion - Implement Accordion component for displaying collapsible content - Add CSS styles for accordion layout and design - Create content.js to hold accordion data

---
+++ Project14/src/App.jsx
@@ -0,0 +1,16 @@
+import Accordion from "./components/Accordion.jsx";
+import { accordionData } from "./utilis/content.js";
+
+const App = () => {
+  return (
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
