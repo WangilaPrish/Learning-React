f73cf5d47c37a44d7c44e02901cc8c76dbd78f33
Priscilla Wangila <nabwonyaprish@gmail.com>
2025-05-28 15:43:46 +0300

    Delete ProfileApp directory

--- ProfileApp/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-const Home = () => (
-    <div className="mt-20 text-center">
-        <h2 className="text-xl font-semibold">Welcome to HuXn WebDev</h2>
-        <p className="text-gray-500">Explore our content to become a better developer!</p>
-    </div>
-);
-
-export default Home;
