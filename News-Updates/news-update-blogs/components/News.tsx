8e16dfdd719c02df86dcbc033f9f63ade259c465
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-16 00:31:39 +0300

    fix: add cursor pointer to buttons in News component for better user experience

--- News-Updates/news-update-blogs/components/News.tsx
+++ News-Updates/news-update-blogs/components/News.tsx
@@ -30,10 +30,10 @@ const News = ({ date, title, description, image }: NewsProps) => {
                     <p className="text-sm">{description}</p>
 
                     <div className="grid grid-cols-2 gap-2 mt-3">
-                        <button className="bg-white text-black text-sm py-1 px-3 rounded hover:bg-gray-200">
+                        <button className="bg-white text-black text-sm py-1 px-3 cursor-pointer rounded hover:bg-gray-200">
                             Read More
                         </button>
-                        <button className="bg-white text-black p-2 rounded flex justify-center items-center hover:bg-gray-200">
+                        <button className="bg-white text-black p-2 rounded flex justify-center cursor-pointer items-center hover:bg-gray-200">
                             <Share2 size={18} />
                         </button>
                     </div>
