0d23905399b99b8793603a395205c23192c5bf0c
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-18 20:54:16 +0300

    feat: add FadeComponent for animated content display

--- Project37/components/FadeComponent.tsx
+++ Project37/components/FadeComponent.tsx
@@ -2,11 +2,17 @@ import { motion } from 'framer-motion'
 
 const FadeComponent = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen">
             <motion.div>
-
+                {/* Your animated content */}
             </motion.div>
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
+            >
+                Click me for Magic!
+            </button>
         </div>
+
     )
 }
 
