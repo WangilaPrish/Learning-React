fa4a37fdd338fb107d8af16edfe7b3b25eedfb0e
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-18 20:48:37 +0300

    feat: initialize Project37 with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.js for Tailwind CSS and autoprefixer integration.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application.
    - Implemented main App component in App.tsx.
    - Added React logo SVG to assets directory.
    - Created global styles in index.css.
    - Set up main entry point in main.tsx.
    - Added Vite environment types in vite-env.d.ts.
    - Configured Tailwind CSS in tailwind.config.js.
    - Created TypeScript configuration files for app and node.
    - Set up Vite configuration in vite.config.ts.

---
+++ Project37/components/FadeComponent.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion'
+
+const FadeComponent = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default FadeComponent
---
+++ Project37/src/App.tsx
@@ -0,0 +1,11 @@
+import FadeComponent from '../components/FadeComponent'
+
+const App = () => {
+    return (
+        <div>
+            <FadeComponent />
+        </div>
+    )
+}
+
+export default App
