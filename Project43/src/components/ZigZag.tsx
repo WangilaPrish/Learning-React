5db8403378b5f4e180817e292bc6411c8d83fe23
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-20 19:25:49 +0300

    feat: implement ZigZag component with framer-motion animations for dynamic movement

--- Project43/src/components/ZigZag.tsx
+++ Project43/src/components/ZigZag.tsx
@@ -2,12 +2,21 @@ import { motion } from 'framer-motion';
 
 const ZigZag = () => {
     return (
-        <div>
-            <motion.div>
-
-            </motion.div>
+        <div className="min-h-screen flex items-center justify-center">
+            <motion.div
+                animate={{
+                    x: [0, 100, 0, -100, 0], // left-right-left
+                    y: [0, -50, -100, -50, 0], // up-down-up
+                }}
+                transition={{
+                    duration: 4,
+                    ease: 'easeInOut',
+                    repeat: Infinity,
+                }}
+                className="w-12 h-20 bg-blue-500"
+            />
         </div>
-    )
-}
+    );
+};
 
-export default ZigZag
+export default ZigZag;
