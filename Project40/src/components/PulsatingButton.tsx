f04b3cd41bfdced81914ef4ef52f369f2a18fbef
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-20 16:10:25 +0300

    feat: enhance content structure in PulsatingButton component

--- Project40/src/components/PulsatingButton.tsx
+++ Project40/src/components/PulsatingButton.tsx
@@ -3,8 +3,14 @@ import { motion } from 'framer-motion';
 const PulsatingButton = () => {
     return (
         <div>
-            <div>
-
+            <div
+                className='text-2xl text-center font-bold mb-4'
+            >
+                Hello guys, welcome and welcome back to my youtube channel.
+                <br />
+                If you new here, make sure to hit that subscribe button and turn on the bells for notifactions.
+                <br />
+                In my community, you'd never be bored as i give you juicy tea.
             </div>
             <motion.button>
 
