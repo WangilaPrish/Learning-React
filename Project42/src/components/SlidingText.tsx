770f1d4a330f2e22569fc7d21f26ece2e13d6a31
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-20 18:07:36 +0300

    feat: enhance SlidingText component with framer-motion animations for smoother transitions

--- Project42/src/components/SlidingText.tsx
+++ Project42/src/components/SlidingText.tsx
@@ -3,8 +3,21 @@ import { motion } from 'framer-motion';
 const SlidingText = () => {
     return (
         <div>
-            <motion.div>
-                <p>Are you seeing me sliding in?
+            <motion.div
+                initial={{ x: '-100%' }}
+                animate={{ x: 260 }}
+                transition={{
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 10,
+                    delay: 0.5,
+                    duration: 1.5
+                }}
+                className='flex bg-teal-400 h-[150px] w-[370px] my-20 rounded-3xl'
+            >
+                <p
+                    className='text-center flex items-center p-20 font-serif font-semibold'
+                >Are you seeing me sliding in?
                     <br />
                     Lol :)
                 </p>
