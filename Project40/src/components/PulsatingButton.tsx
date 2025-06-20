7378d2eed495b8f7006a6d17caa5f28cc35e95a3
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-20 16:13:29 +0300

    feat: update PulsatingButton component styles and structure

--- Project40/src/components/PulsatingButton.tsx
+++ Project40/src/components/PulsatingButton.tsx
@@ -4,7 +4,7 @@ const PulsatingButton = () => {
     return (
         <div>
             <div
-                className='text-2xl text-center font-bold mb-4'
+                className='text-2xl text-center font-bold m-8'
             >
                 Hello guys, welcome and welcome back to my youtube channel.
                 <br />
@@ -12,8 +12,10 @@ const PulsatingButton = () => {
                 <br />
                 In my community, you'd never be bored as i give you juicy tea.
             </div>
-            <motion.button>
-
+            <motion.button
+                className='bg-blue-500 text-white px-6 py-3 rounded-full m-20 items-center flex'
+            >
+                Subscribe
             </motion.button>
         </div>
     )
