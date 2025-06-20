7c66ee5ff6303f89fe63f038256671755a857913
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-20 16:04:27 +0300

    feat: add image to BouncingBall component and enhance button size and animation

--- Project39/src/components/BouncingBall.tsx
+++ Project39/src/components/BouncingBall.tsx
@@ -4,18 +4,25 @@ const BouncingBall = () => {
     return (
         <div className="flex justify-center items-center min-h-screen">
             <motion.button
-                className="bg-blue-500 text-white p-4 rounded-full h-[50px] w-[50px]"
+
+                className="bg-blue-500 text-white p-4 rounded-full h-[350px] w-[250px]"
                 animate={{
-                    y: [0, -30, 0],
-                    x: [0, 30, 0],
+                    y: [0, -60, 0],
+                    x: [0, 60, 0],
                 }}
                 initial={{ y: 0, x: 0 }}
                 transition={{
-                    duration: 0.8,
+                    duration: 0.4,
                     repeat: Infinity,
                     ease: 'easeInOut',
                 }}
-            />
+            >
+                <img
+                    src={'src/assets/mike.jpg'}
+                    alt="Preview"
+                    className="w-full h-full object-cover rounded-full"
+                />
+            </motion.button>
         </div>
     );
 };
