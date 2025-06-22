3d078ce53bdc342f6be0462b0e92c79603eed617
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-22 17:40:54 +0300

    feat: add BackgroundAnimation component with framer-motion integration

---
+++ Project45/src/components/BackgroundAnimation.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const BackgroundAnimation = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default BackgroundAnimation
