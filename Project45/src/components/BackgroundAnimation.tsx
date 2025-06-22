3692acbef81007b947a0b2d082f07ea887da6d94
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-22 19:50:31 +0300

    feat: add background color animation to utilities in index.css

--- Project45/src/components/BackgroundAnimation.tsx
+++ Project45/src/components/BackgroundAnimation.tsx
@@ -3,7 +3,9 @@ import { motion } from 'framer-motion';
 const BackgroundAnimation = () => {
     return (
         <div>
-            <motion.div>
+            <motion.div
+                className='bg-neutral-900 h-full w-full '
+            >
 
             </motion.div>
         </div>
