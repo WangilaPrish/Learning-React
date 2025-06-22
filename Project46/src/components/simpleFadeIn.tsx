2357bcce0b4ec8f7ea586b7acc38839c636caa78
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-22 20:11:25 +0300

    feat: add simpleFadeIn component with framer-motion integration

---
+++ Project46/src/components/simpleFadeIn.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const simpleFadeIn = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default simpleFadeIn
