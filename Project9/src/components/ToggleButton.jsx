f52fad15c1ccf1f6bff669aee1729b981a779b1a
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-23 22:26:16 +0300

    fix: update button text in ToggleButton from "Click me" to "Click me"

--- Project9/src/components/ToggleButton.jsx
+++ Project9/src/components/ToggleButton.jsx
@@ -12,7 +12,7 @@ const ToggleButton = () => {
                 setBackgroundColor(backgroundColor === 'black' ? 'white' : 'black')
                 setTextColor(textColor === 'white' ? 'black' : 'white')
                 setButtonColor(buttonColor === 'red' ? 'white' : 'red')
-            }}>Click mvegvbnerh</button>
+            }}>Click me</button>
       </div>
   )
 }
