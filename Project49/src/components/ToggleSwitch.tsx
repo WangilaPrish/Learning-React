146944c2f06ff57e21acc04c4a466028e0494244
WangilaPrish <nabwonyaprish@gmail.com>
2025-06-24 17:35:23 +0300

    feat: add ToggleSwitch component with framer-motion integration

---
+++ Project49/src/components/ToggleSwitch.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion'
+
+const ToggleSwitch = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default ToggleSwitch
