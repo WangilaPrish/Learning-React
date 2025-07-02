76d5bdc52bd706fb94188e30a4b07d9f129f3def
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-02 13:16:07 +0300

    feat: implement DraggableBox component with drag functionality

--- Project54/src/components/DraggableBox.tsx
+++ Project54/src/components/DraggableBox.tsx
@@ -1,8 +1,18 @@
-
+import { motion } from 'framer-motion';
 
 const DraggableBox = () => {
     return (
-        <div>DraggableBox</div>
+        <div className="flex items-center justify-center min-h-screen bg-teal-500">
+            <motion.div
+                className='bg-white w-64 h-64 rounded-lg'
+                drag
+                dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}
+                initial={{ backgroundColor: '#fff' }}
+                whileDrag={{ backgroundColor: 'blue' }}
+            >
+
+            </motion.div>
+        </div>
     )
 }
 
