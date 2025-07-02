c920cb291b06817468181ae881fb67cff78adaf7
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-02 13:56:03 +0300

    feat: implement initial structure for TaptochangeColor component with framer-motion

--- Project56/src/components/TaptochangeColor.tsx
+++ Project56/src/components/TaptochangeColor.tsx
@@ -2,8 +2,10 @@ import { motion } from 'framer-motion';
 
 const TaptochangeColor = () => {
     return (
-        <div>
-            <motion.div>
+        <div className='flex justify-center items-center h-screen bg-gray-100'>
+            <motion.div
+                className='w-64 h-64 bg-blue-500 rounded-lg'
+            >
 
             </motion.div>
         </div>
