c03cef96d58a3101ba2b7c5e0c37d0423b078b9f
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-02 20:18:03 +0300

    feat: add StaggeredButtonPress component and integrate it into App

--- Project61/src/App.tsx
+++ Project61/src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import StaggeredButtonPress from "./components/StaggeredButtonPress"
+
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <StaggeredButtonPress />
+        </div>
     )
 }
 
---
+++ Project61/src/components/StaggeredButtonPress.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StaggeredButtonPress = () => {
+    return (
+        <div>StaggeredButtonPress</div>
+    )
+}
+
+export default StaggeredButtonPress
