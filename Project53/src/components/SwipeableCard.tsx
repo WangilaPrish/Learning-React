ab838301920944c851ed7f813b864764cff20592
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-02 13:00:51 +0300

    feat: add drag functionality to SwipeableCard component

--- Project53/src/components/SwipeableCard.tsx
+++ Project53/src/components/SwipeableCard.tsx
@@ -3,9 +3,21 @@ import { motion } from 'framer-motion';
 const SwipeableCard = () => {
     return (
         <div>
-            <motion.div>
-
+            <motion.div
+                drag="x"
+                onDragEnd={(_, info) => {
+                    if (info.offset.x > 100) {
+                        console.log('Swiped right!');
+                    } else if (info.offset.x < -100) {
+                        console.log('Swiped left!');
+                    }
+                }}
+            >
+                <div className="p-4 rounded-lg shadow bg-white">
+                    <p>Swipe me!</p>
+                </div>
             </motion.div>
+
         </div>
     )
 }
