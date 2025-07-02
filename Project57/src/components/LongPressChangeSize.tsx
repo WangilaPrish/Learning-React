107b867a7f22a3445b172691b9a80052280b2434
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-02 14:22:34 +0300

    feat: remove unused App.css and refactor App component to use LongPressChangeSize

---
+++ Project57/src/components/LongPressChangeSize.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const LongPressChangeSize = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default LongPressChangeSize
