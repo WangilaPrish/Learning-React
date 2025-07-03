ffe0ebf4029166e2013c5375f682ad731092c6f4
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-03 19:43:47 +0300

    feat: add StaggeredTextReveal component with initial structure

---
+++ Project63/src/components/StaggeredTextReveal.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const StaggeredTextReveal = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default StaggeredTextReveal
