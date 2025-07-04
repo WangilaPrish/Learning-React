371bad73748c44625b08a6218d7436fcd634c83c
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-04 21:11:01 +0300

    feat: remove unused App.css and refactor App component structure feat: add StaggeredFadeInSlideIn component for animation effects

---
+++ Project68/src/components/StaggeredFadeInSlideIn.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const StaggeredFadeInSlideIn = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default StaggeredFadeInSlideIn
