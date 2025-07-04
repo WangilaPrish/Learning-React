2207e70a95ad4206c90137cca33c436dd0b8e66b
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-04 21:52:51 +0300

    feat: replace App component content with ComplexTimelineAnimation component

---
+++ Project70/src/components/ComplexTimelineAnimation.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+const ComplexTimelineAnimation = () => {
+    return (
+        <div>
+            <motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default ComplexTimelineAnimation
